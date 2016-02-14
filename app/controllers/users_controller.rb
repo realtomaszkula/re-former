b6092cfff5e9ce0422229a9924f44996d7a318cf
Tomasz Kula <tomaszkula07@gmail.com>
2016-02-14 18:26:57 +0100

    Add Error msg and flash notice

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:notice] = "User successfully edited"
+      redirect_to edit_user_path
+    else
+      render :edit
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:notice] = "User successfully created"
       redirect_to new_user_path
     else
       render :new
